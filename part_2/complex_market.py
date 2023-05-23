9de42fc2c26413c52ee90a1168e54f4b00a97b49
bohdanhlovatskyi <bohdan.hlovatskyi@ucu.edu.ua>
2023-05-23 13:49:57 +0300

    final

--- part_2/complex_market.py
+++ part_2/complex_market.py
@@ -29,24 +29,3 @@ class MarketEnv:
     
         return (self.t, self.prev_price), profit, self.t == 53
 
-
-# It will be useful to use inheritance to implement agents for both types of the environment.
-# Since tabular Q-learning deals with finite state-space, the agent should discretize continious observations.
-class MarketAgent:
-    def __init__(self, min_price, max_price, bins_number, learning_rate=0.1, discount_factor=0.96,
-                 exploration_rate=0.98, exploration_decay_rate=0.99):
-        # TODO: Your code here
-        pass
-    
-    def begin_episode(self, observation):
-        # TODO: Your code here. Implement method that returns an epsilon-greedy action.
-        pass
-    
-    def act(self, observation, reward, done):
-        # TODO: Your code here. Implement method that returns an epsilon-greedy action and updates Q-table.
-        pass
-    
-    def get_optimal_action(self, observation):
-        # TODO: Your code here. Implement method that returns an optimal action.
-        # This method will be used to compare performance with greedy agent.
-        pass
--- part_2/simple_market.py
+++ part_2/simple_market.py
@@ -16,19 +16,3 @@ class MarketEnv:
         self.t += 1
 
         return self.t, profit, self.t == 53
-
-
-# Since tabular Q-learning deals with finite state-space, the agent should discretize continious observations.
-class MarketAgent:
-    def __init__(self, min_price, max_price, bins_number, learning_rate=0.1, discount_factor=0.96,
-                 exploration_rate=0.98, exploration_decay_rate=0.99):
-        # TODO: Your code here
-        pass
-    
-    def begin_episode(self, observation):
-        # TODO: Your code here. Implement method that returns an epsilon-greedy action.
-        pass
-    
-    def act(self, observation, reward, done):
-        # TODO: Your code here. Implement method that returns an epsilon-greedy action and updates Q-table.
-        pass
